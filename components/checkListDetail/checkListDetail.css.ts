0f0726344174fd880437121ea7e7bca8bf3a047c
taeyuls <startez03@gmail.com>
2024-07-15 23:37:38 +0900

    리드미 수정

--- components/checkListDetail/checkListDetail.css.ts
+++ components/checkListDetail/checkListDetail.css.ts
@@ -2,35 +2,35 @@ import { vars } from "@/app/globals.css";
 import { style } from "@vanilla-extract/css";
 
 export const checkListNameContainer = style({
-  width: '100%',
-  height: '64px',
-  borderRadius: '24px',
+  width: "100%",
+  height: "64px",
+  borderRadius: "24px",
   border: `2px solid ${vars.slate900}`,
-  padding: '16px 0',
-  marginTop:'24px',
-  boxSizing:'border-box',
-  display: 'flex',
+  padding: "16px 0",
+  marginTop: "24px",
+  boxSizing: "border-box",
+  display: "flex",
   justifyContent: "center",
-  alignItems: 'center',
-  backgroundColor:"white"
-})
+  alignItems: "center",
+  backgroundColor: "white",
+});
 
 export const checkListNameContainerCompleted = style({
-  backgroundColor: vars.violet100, // 원하는 색상으로 변경
+  backgroundColor: vars.violet100,
 });
 
 export const checkListName = style({
-  WebkitBoxSizing: 'content-box',
-  MozBoxSizing: 'content-box',
-  boxSizing:'border-box',
+  WebkitBoxSizing: "content-box",
+  MozBoxSizing: "content-box",
+  boxSizing: "border-box",
   outline: "none",
   border: "none",
-  fontFamily: 'NanumSquareR',
-  fontSize: '16px',
-  cursor: 'pointer',
-  backgroundColor:'transparent'
-})
+  fontFamily: "NanumSquareR",
+  fontSize: "16px",
+  cursor: "pointer",
+  backgroundColor: "transparent",
+});
 
 export const checkIcon = style({
-  marginRight: '16px',
-})
+  marginRight: "16px",
+});
